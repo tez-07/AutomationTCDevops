e30d2251acb577e6d521d75fcb101a3ac5317123
Tez Borgohain <tezborgohain1@gmail.com>
2025-12-01 13:13:18 -0500

    Initial Commit

---
+++ src/test/java/Academy/BrowserTest.java
@@ -0,0 +1,29 @@
+package Academy;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+public class BrowserTest {
+
+	
+	@Test
+	public void getData()
+	{
+		System.out.println("Hello Guys");
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		WebDriver driver =new ChromeDriver();
+		driver.get("https://tezborgohain11-gyd0b0a0aaehfsc9.canadacentral-01.azurewebsites.net/webapp/");
+		String text =driver.findElement(By.cssSelector("h1")).getText();
+		System.out.println(text);
+		Assert.assertTrue(text.equalsIgnoreCase("RahulShettyAcademy.com Learning"));
+		driver.close();
+	
+		
+		
+	}
+}
