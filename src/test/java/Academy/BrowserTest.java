30d147a73d97702602f8bfd3c2df4857b956f44c
Tez Borgohain <tezborgohain1@gmail.com>
2025-12-01 20:44:40 -0500

    Latest Updated code

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -17,7 +17,7 @@ public class BrowserTest {
 		System.out.println("Hello Guys");
 		 
 		WebDriver driver =new ChromeDriver();
-		driver.get("https://tezborgohain11-gyd0b0a0aaehfsc9.canadacentral-01.azurewebsites.net/webapp/");
+		driver.get("https://tborgohainqa1.azurewebsites.net/webapp/");
 		String text =driver.findElement(By.cssSelector("h1")).getText();
 		System.out.println(text);
 		Assert.assertTrue(text.equalsIgnoreCase("Azure Devops with Automation"));
