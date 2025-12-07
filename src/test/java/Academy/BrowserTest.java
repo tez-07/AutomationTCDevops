d672d549c45fab5d4578c7be95879dee3c67ffc9
Tez Borgohain <tezborgohain1@gmail.com>
2025-12-07 15:58:30 -0500

    Update BrowserTest.java

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -1,42 +1,26 @@
-package Academy;
-
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BrowserTest {
 
     @Test
     public void getData() {
-        // Set path to your ChromeDriver executable
-        System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
+        WebDriverManager.chromedriver().setup(); // automatically downloads correct driver
 
-        // Set Chrome options
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless"); // Headless mode
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
 
-        // Initialize WebDriver
         WebDriver driver = new ChromeDriver(options);
+        driver.get("https://tborgohainqa01.azurewebsites.net/webapp/");
 
-        try {
-            // Open the URL
-            driver.get("https://tborgohainqa01.azurewebsites.net/webapp/");
+        String text = driver.findElement(By.cssSelector("h1")).getText();
+        Assert.assertEquals("Azure DevOps with Automation", text);
 
-            // Get the text from the <h1> element
-            String text = driver.findElement(By.cssSelector("h1")).getText();
-            System.out.println("Header text: " + text);
-
-            // Assert the text
-            Assert.assertEquals("Azure DevOps with Automation", text);
-
-        } finally {
-            // Quit the driver to close all sessions
-            driver.quit();
-        }
+        driver.quit();
     }
 }
